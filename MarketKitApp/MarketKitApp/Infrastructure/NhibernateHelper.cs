beb12e3312f6a02965b1748864b7bd0eb08680a4
Ashies Paul <ashies.paul@gmail.com>
2012-07-17 15:12:53 +0530

    nhibernate helper

---
+++ MarketKitApp/MarketKitApp/Infrastructure/NhibernateHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate;
+using NHibernate.Cfg;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+
+namespace MarketKitApp.Infrastructure
+{
+    public class NhibernateHelper
+    {
+        private const string CurrentSessionKey = "nhibernate.current_session";
+        private static readonly ISessionFactory sessionFactory;
+
+        static NhibernateHelper()
+        {
+            sessionFactory = Fluently.Configure()
+                        .Database(MsSqlConfiguration.MsSql2005.ConnectionString(c => c.FromConnectionStringWithKey("mycon")))
+                        .Mappings(m =>
+                          m.FluentMappings.AddFromAssemblyOf<NhibernateHelper>())
+                        .BuildSessionFactory();
+        }
+
+        //private static ISessionFactory CreateSessionFactory()
+        //{
+        //    return Fluently.Configure()
+        //       .BuildSessionFactory();
+        //}
+
+        public static ISession GetCurrentSession()
+        {
+            HttpContext context = HttpContext.Current;
+            ISession currentSession = context.Items[CurrentSessionKey] as ISession;
+
+            if (currentSession == null)
+            {
+                currentSession = sessionFactory.OpenSession();
+                context.Items[CurrentSessionKey] = currentSession;
+            }
+
+            return currentSession;
+        }
+
+        public static void CloseSession()
+        {
+            HttpContext context = HttpContext.Current;
+            ISession currentSession = context.Items[CurrentSessionKey] as ISession;
+
+            if (currentSession == null)
+            {
+                // No current session
+                return;
+            }
+
+            currentSession.Close();
+            context.Items.Remove(CurrentSessionKey);
+        }
+
+        public static void CloseSessionFactory()
+        {
+            if (sessionFactory != null)
+            {
+                sessionFactory.Close();
+            }
+        }
+
+    }
+}
